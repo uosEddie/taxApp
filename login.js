566b7c27c32ec25a63acedc61e4399416b2103aa
Edmund <edmund@Edmunds-MacBook-Air.local>
2026-05-12 22:46:35 +0100

    fix backend url

--- login.js
+++ login.js
@@ -12,7 +12,7 @@ loginForm.addEventListener("submit", function(event){
         userPassword: loginPassword.value
     };
 
-    fetch("https://taxapp-production-940c.up.railway.app/register", {
+    fetch("https://taxapp-production-940c.up.railway.app/login", {
         method: "POST",
 
         headers: {
