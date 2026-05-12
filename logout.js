f3ce227cb221321e2113c70cae0bf8aa9c3a1556
Edmund <edmund@Edmunds-MacBook-Air.local>
2026-05-12 21:50:31 +0100

    fix api urls

--- logout.js
+++ logout.js
@@ -1,17 +1,28 @@
-let currentUserToDelete = localStorage.getItem("Current User");
-
 let logOutBtn = document.getElementById("logout-btn");
 let mobileLogOutBtn = document.getElementById("mobile-logout-btn");
 
 
-logOutBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    localStorage.removeItem("Current User");
-    window.location.href = "index.html";
-});
+if(logOutBtn){
 
-mobileLogOutBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    localStorage.removeItem("Current User");
-    window.location.href = "index.html";
-});
+    logOutBtn.addEventListener("click", function(event){
+        event.preventDefault();
+
+        localStorage.removeItem("Current User");
+
+        window.location.href = "index.html";
+    });
+
+}
+
+
+if(mobileLogOutBtn){
+
+    mobileLogOutBtn.addEventListener("click", function(event){
+        event.preventDefault();
+
+        localStorage.removeItem("Current User");
+
+        window.location.href = "index.html";
+    });
+
+}
