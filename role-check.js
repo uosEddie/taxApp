7e2a65bb8c0bfd6d3005bad97f08d13874eb8763
Edmund <edmund@Edmunds-MacBook-Air.local>
2026-05-13 18:51:28 +0100

    Div

--- role-check.js
+++ role-check.js
@@ -1,11 +1,12 @@
-let currentUserText = localStorage.getItem("Current User");
-let currentUser = JSON.parse(currentUserText);
+let savedCurrentUser = localStorage.getItem("Current User");
+let currentUser = JSON.parse(savedCurrentUser);
 
 if(currentUser.role === "Taxpayer"){
-   showMessage("Access denied. Taxpayers cannot access this page.", "warning");
 
-setTimeout(function(){
-    window.location.href = "dashboard.html";
-}, 3000);
-    window.location.href = "dashboard.html";
+    showMessage("Access denied. Taxpayers cannot access this page.", "warning");
+
+    setTimeout(function(){
+        window.location.href = "dashboard.html";
+    }, 3000);
+
 }
